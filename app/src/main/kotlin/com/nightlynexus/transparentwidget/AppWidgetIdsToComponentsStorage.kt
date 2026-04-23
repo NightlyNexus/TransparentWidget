db4ff2285f82865167f89755cbd8b6ab3895d516
Eric Cochran <Eric@NightlyNexus.com>
2026-04-23 13:40:36 -0400

    Fall back to a blank widget if the user manually edits his SharedPreferences file.

--- app/src/main/kotlin/com/nightlynexus/transparentwidget/AppWidgetIdsToComponentsStorage.kt
+++ app/src/main/kotlin/com/nightlynexus/transparentwidget/AppWidgetIdsToComponentsStorage.kt
@@ -2,6 +2,7 @@ package com.nightlynexus.transparentwidget
 
 import android.content.ComponentName
 import android.content.SharedPreferences
+import androidx.core.content.edit
 
 internal class AppWidgetIdsToComponentsStorage(private val sharedPreferences: SharedPreferences) {
   fun setComponent(appWidgetId: Int, componentName: ComponentName?) {
@@ -10,18 +11,21 @@ internal class AppWidgetIdsToComponentsStorage(private val sharedPreferences: Sh
     } else {
       "${componentName.packageName}\n${componentName.className}"
     }
-    sharedPreferences.edit().putString(appWidgetId.toString(), value).apply()
+    sharedPreferences.edit { putString(appWidgetId.toString(), value) }
   }
 
   fun remove(appWidgetId: Int) {
-    sharedPreferences.edit().remove(appWidgetId.toString()).apply()
+    sharedPreferences.edit { remove(appWidgetId.toString()) }
   }
 
   fun getComponentName(appWidgetId: Int): ComponentName? {
-    val value = sharedPreferences.getString(appWidgetId.toString(), null) ?: return null
+    val key = appWidgetId.toString()
+    val value = sharedPreferences.getString(key, null) ?: return null
     val index = value.indexOf('\n')
     if (index == -1) {
-      throw IllegalStateException(value)
+      // The user manually edited his SharedPreferences file contents.
+      // Make the widget blank as though it has no ComponentName.
+      return null
     }
     val packageName = value.substring(0, index)
     val className = value.substring(index + 1)
